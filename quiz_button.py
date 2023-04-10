d0e3b70c555161f4a359f41de5133cdc3594439e
Abdullah Azeez <112954459+Asurola@users.noreply.github.com>
2023-04-10 14:02:22 +0800

    Update quiz_button.py

--- quiz_button.py
+++ quiz_button.py
@@ -3,7 +3,7 @@ def make_quiz():
     quiz_window = tk.Tk()
     quiz_window.title(text="Quiz")
     quiz_window.geometry("400x300")
-    print('hi')
+    print('balls')
 main_window = tk.Tk()
 main_window.geometry("400x300")
 the_button = tk.Button(main_window,text="Quiz",command=make_quiz)
